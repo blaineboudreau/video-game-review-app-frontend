c797ddc71664c497c832c33879374cda45dd1c91
blaineboudreau <blaine.boudreau@gmail.com>
2017-02-02 14:26:16 -0600

    frontend running on server, deployed heroku

--- video_game_reviews_app_frontend/server.js
+++ video_game_reviews_app_frontend/server.js
@@ -1,16 +1,15 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
-var pg = require('pg')
+var pg = require('pg');
 
 var mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/call_of_review_app';
 
 
 mongoose.connect(mongoUri)
-app.use(express.static('public'));
 
 port = process.env.PORT || 3000;
-
+app.use(express.static('public'));
 
 
 
