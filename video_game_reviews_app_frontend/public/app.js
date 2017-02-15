754014c55a07c8573a291f1336743f52a32336ea
blaineboudreau <blaine.boudreau@gmail.com>
2017-02-14 18:07:35 -0600

    styling

--- video_game_reviews_app_frontend/public/app.js
+++ video_game_reviews_app_frontend/public/app.js
@@ -183,8 +183,24 @@ this.getUsers = function() {
 window.onload=function() {
 
   document.getElementById("new").style.display="none";
+  // document.getElementById("edit").style.display="none";
+  document.getElementById("sin").style.display="none";
+  document.getElementById("log").style.display="none";
+
 }
 
 show = function() {
     document.getElementById("new").style.display="block";
+}
+
+showEdit = function() {
+    document.getElementById("edit").style.display="block";
+}
+
+showSin = function() {
+    document.getElementById("sin").style.display="block";
+}
+
+showLog = function() {
+    document.getElementById("log").style.display="block";
 }
