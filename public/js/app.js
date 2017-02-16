0189d5132a72d78b5950a435e38c86b329f7a508
blaineboudreau <blaine.boudreau@gmail.com>
2017-02-16 04:51:52 -0600

    heroku

--- public/js/app.js
+++ public/js/app.js
@@ -16,7 +16,9 @@ app.controller('mainController', ['$http', function($http) {
 
   var localEnv = false; // true/localhost, false/heroku
 
-  if (localEnv) { var url = 'http://localhost:3000'} else { var url = 'https://call-of-review-frontend.herokuapp.com' }
+  // if (localEnv) { var url = 'http://localhost:3000'} else { var url = 'https://call-of-review-frontend.herokuapp.com' }
+
+  if (localEnv) { var url = 'http://localhost:3000'} else { var url = 'https://call-of-review-backend.herokuapp.com' }
 
 
 //----------------------login functionality------------------------------------------------------------------------------------
